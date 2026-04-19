ce925b774bdf69ed2878faa8f197b9e5e3e91394
gabriel baima <g43r@hotmail.com>
2026-04-19 17:05:52 -0300

    Mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("decrição curso java");
+        curso1.setCargaHoraria(15);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("decrição curso JS");
+        curso1.setCargaHoraria(10);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
